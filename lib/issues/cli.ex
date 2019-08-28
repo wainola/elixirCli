dee4ed56bf4db2c37f815111f65070fb55b3a925
Nicolás Riquelme <nrriquelme@gmail.com>
2019-08-27 21:05:19 -0400

    Adding sort_into_descending_order function

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -21,8 +21,15 @@ defmodule Issues.CLI do
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
   end
 
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
+
+  end
+  
   def decode_response({:ok, body}), do: body
 
   def decode_response({:error, error}) do
